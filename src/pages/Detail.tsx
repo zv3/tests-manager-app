81c07b3f5642f1941bf62e09856f1db1aabe1717
Diego Zacarias <511910+zv3@users.noreply.github.com>
2022-11-07 01:50:58 -0300

    Adding the ability to edit the name of a test suite

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,14 +1,17 @@
 import {useNavigate, useParams} from 'react-router-dom';
-import { useContext } from 'react';
+import {ChangeEvent, SyntheticEvent, useContext, useState} from 'react';
+import cloneDeep from 'lodash/cloneDeep';
+import { Label, TextInput, Button } from 'flowbite-react'
 import ChevronUp from '../assets/chevron-up.svg';
 import AppContext from '../appContext';
+import {TestSuite} from "../models/TestSuite";
 
 type SuiteDetailPageParams = {
   suiteId: string;
 };
 
 const Detail = () => {
-  const context = useContext(AppContext);
+  const { testSuites, setTestSuites } = useContext(AppContext);
   const params = useParams<SuiteDetailPageParams>();
   const navigate = useNavigate();
 
@@ -17,13 +20,41 @@ const Detail = () => {
   }
 
   const suiteId = parseInt(params.suiteId);
-  const suite = suiteId && context.testSuites.find((suite) => suite.id === suiteId);
+  const suite = suiteId && testSuites.find((suite) => suite.id === suiteId);
   if (!suite) {
     throw Error(`Invalid 'suiteId' parameter with value '${suiteId}'`);
   }
 
-  const onClickBackButton = () => {
+  const [model, setModel] = useState<TestSuite>(cloneDeep(suite));
+
+  const navigateBack = () => {
     navigate('/');
+  }
+
+  const onClickBackButton = () => {
+    navigateBack()
+  }
+
+  const onChangeNameInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setModel({
+      ...model,
+      name: event.target.value
+    });
+  }
+
+  const onSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    const nextTestSuites = testSuites.map(entry => {
+      if (entry.id === suite.id) {
+        return model;
+      }
+
+      return entry;
+    });
+
+    setTestSuites(nextTestSuites);
+    navigateBack();
   }
 
   return (
@@ -34,9 +65,23 @@ const Detail = () => {
           </button>
 
         <div className="absolute left-1/2 top-0 -translate-x-1/2">
-          {suite.name}
+          Edit Test Suite (ID: {model.id})
         </div>
       </header>
+
+      <main>
+        <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+          <div>
+            <div className="mb-2">
+              <Label htmlFor="name" value="Name" />
+            </div>
+
+            <TextInput id="name" type="text" required={true} onInput={onChangeNameInput} value={model.name} />
+          </div>
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </main>
     </div>
   );
 };
